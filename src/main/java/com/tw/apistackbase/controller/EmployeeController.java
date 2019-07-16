6130006ff74324fa8a4c11f785c3c261e6d3a79a
LILY KAN (ITA-ISDC-ISD-OOCLL/ZHA) <KANLI2@OOCL.COM>
2019-07-16 16:20:39 +0800

    add Employee

---
+++ src/main/java/com/tw/apistackbase/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.tw.apistackbase.controller;
+
+import com.tw.apistackbase.model.Employees;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * Created by jxzhong on 18/08/2017.
+ */
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final Logger log = Logger.getLogger(this.getClass().getName());
+
+    @GetMapping
+    public List<Employees> getEmployees() {
+
+        return Employees.createEmployees();
+    }
+
+
+}
---
+++ src/main/java/com/tw/apistackbase/model/Employees.java
@@ -0,0 +1,62 @@
+package com.tw.apistackbase.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employees {
+    private  int id;
+    private String name;
+    private  int age;
+    private  String gender;
+
+    public Employees(int id, String name, int age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public static List<Employees> createEmployees() {
+        List<Employees>employee=new ArrayList<>();
+        Employees employees=new Employees(0,"Xiaoming",20,"Male");
+        employee.add(employees);
+        employee.add(new Employees(1,"Xiaohong",19,"Female"));
+        employee.add(new Employees(2,"Xiaozhi",15,"Male"));
+        employee.add(new Employees(3,"Xiaogang",16,"Male"));
+        employee.add(new Employees(4,"Xiaoxia",15,"Female"));
+        return  employee;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
